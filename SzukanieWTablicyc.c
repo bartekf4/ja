78ac4d0cd69f3bf717dfd0da43b36c866a82f71f
bartekf4 <56221703+bartekf4@users.noreply.github.com>
2020-10-25 16:23:42 +0100

    Add files via upload

---
+++ SzukanieWTablicyc.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+
+int main() {
+	int tablica[] = { 1,2,3,4,5 };
+	int szukana = 4;
+
+	for (int i = 0; i < sizeof(tablica) / sizeof(tablica[0]); ++i) {
+		if (tablica[i] == szukana) {
+			printf("%d\n", tablica[i]);
+		}
+	}
+	return 0;
+}
