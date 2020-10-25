086176ec273049d93992fe1953712eabcd7efdfa
bartekf4 <56221703+bartekf4@users.noreply.github.com>
2020-10-25 16:21:55 +0100

    Add files via upload
    Sprawdzanie czy dwa dane stringi są takie same

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    char slowo[] = "voldemort";
+
+    char wzorzec[] = "voldemort";
+
+
+    int i, j;
+
+    i = sizeof(slowo) / sizeof(slowo[0]);
+    j = sizeof(wzorzec) / sizeof(wzorzec[0]);
+
+
+    if (i == j)
+    {
+        for (int k = 0; i <= j; ++k)
+        {
+            if (slowo[k] == wzorzec[k])
+            {
+                if (k == j) 
+                {
+                    printf("Sa takie same");
+                    break;
+
+                }
+                
+            }
+            else 
+            {
+                printf("Nie sa takie same");
+                break;
+            }
+        }
+
+    }
+    else
+        printf("Maja inna dlugosc");
+
+}
